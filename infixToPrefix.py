8a0d345d4e042a0fc396b59c52174880c362c3da
naglicv <vn9344@student.uni-lj.si>
2023-11-06 17:58:24 +0100

    Added script for infix to prefix conversion

---
+++ infixToPrefix.py
@@ -0,0 +1,41 @@
+def infix_to_prefix(infix):
+    # Function to convert infix to prefix expression
+    def precedence(op):
+        # Returns the precedence of the operator
+        if op in ['+', '-']:
+            return 1
+        if op in ['*', '/']:
+            return 2
+        if op in ['**']:
+            return 3
+        return 0
+
+    def is_operator(op):
+        # Returns true if the operator is an operator
+        return op in ['+', '-', '*', '/', '**']
+
+    stack = []
+    output = []
+    for token in reversed(infix):
+        if is_operator(token):
+            while stack and precedence(token) < precedence(stack[-1]):
+                output.append(stack.pop())
+            stack.append(token)
+        elif token == ')':
+            stack.append(token)
+        elif token == '(':
+            while stack[-1] != ')':
+                output.append(stack.pop())
+            stack.pop()
+        else:
+            output.append(token)
+
+    while stack:
+        output.append(stack.pop())
+
+    output.reverse()
+    return output
+
+if __name__ == '__main__':
+    s = "(x+y)*z/w+u"
+    print(infix_to_prefix(s))
