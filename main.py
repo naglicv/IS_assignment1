2e72d741a466e66fcb73b9129403ac87feb93ba5
ArjanSkok <arjan.skok@gmail.com>
2023-11-16 11:33:36 +0100

    updated main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import matplotlib
 import pygad
 import numpy as np
 from scipy.stats import norm
+import treebuild as tb
+import infixToPrefix
 
 #fitness function
 def fitness(ga_instance, solution, solution_idx):
@@ -13,9 +15,17 @@ def fitness(ga_instance, solution, solution_idx):
 
 
 data = pd.read_csv("./datasets/dataset.csv", sep=",")
-equation = data['Equation']
+equations = data['Equation']
 Xs = data['Xs']
 Ys = data['Ys']
+
+
+for equation in equations:
+    eq = infixToPrefix.infix_to_prefix(equation)
+    print(infixToPrefix.infix_to_prefix(equation))
+    #drevo = tb.gradi_drevo(eq)
+    #tb.printTree(drevo)
+    print('\n')
 
 
 
