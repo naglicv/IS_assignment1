fd7b94ea3dd218b9644e83110a436f177bcbfce5
ArjanSkok <arjan.skok@gmail.com>
2023-11-24 11:51:48 +0100

    Created generateTree functionality

--- treebuild.py
+++ treebuild.py
@@ -7,6 +7,8 @@ import seaborn as sns
 import infixToPrefix as itp
 import re
 
+const = 1.e-10
+
 #ustvari strukturo node (vozlišče) drevesa
 class Node:
     def __init__(self, value):
@@ -31,10 +33,37 @@ def printTree(root):
         printTree(root.left)
         printTree(root.right)
 
-def countTree(root):
-    count = 0
-    if root != None:
-        count += 1
-        count += countTree(root.left)
-        count += countTree(root.right)
-    return count
+
+def generateExpression(globina):
+    rng = np.random.default_rng()
+    operatorji = ['+', '-', '*', '/', '^']
+    operandi = list(range(-10,10))
+    spremenljivka = ['x', '-x']
+    rand = int(rng.random()*10) + (0.2 * globina)
+    if rand <= 5:
+        expression = operatorji[int(rng.random() * 5 - const)]
+    elif rand <= 7:
+        expression = spremenljivka[int(rng.random() * 2 - const)]
+    else:
+        expression = str(operandi[int(rng.random() * 19 - const)])
+    return expression
+
+
+
+def generateTree(globina):
+    expression = generateExpression(globina)
+    root = Node(expression)
+    if root.value.lstrip('-+').isnumeric() or root.value.lstrip('-+').isalpha():
+        return root
+    root.left = generateTree(globina+1)
+    root.right = generateTree(globina+1)
+    return root
+
+
+X = []
+for i in range (20):
+    drevo = generateTree(0)
+    X.append(drevo)
+    printTree(drevo)
+    print()
+    
